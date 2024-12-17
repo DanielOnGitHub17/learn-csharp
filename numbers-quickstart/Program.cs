ba37ceeef6ad64cb7e0675fa89aea1d97f87c405
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2024-12-17 22:35:38 +0000

    Implement basic arithmetic operations in the console application

--- numbers-quickstart/Program.cs
+++ numbers-quickstart/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿WorkWithIntegers();
+
+void WorkWithIntegers()
+{
+    // addition
+    int a = 18;
+    int b = 6;
+    int c = a + b;
+    Console.WriteLine(c);
+
+    // subtraction
+    c = a - b;
+    Console.WriteLine(c);
+
+    // multiplication
+    c = a * b;
+    Console.WriteLine(c);
+
+    // division
+    c = a / b;
+    Console.WriteLine(c);
+}
