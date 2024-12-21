c8e1046453c134dfdd0bf8aa8f7d23662bfdb940
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2024-12-20 21:21:49 -0600

    moved folders to learning dir

---
+++ learning/list-tutorial/Program.cs
@@ -0,0 +1,67 @@
+﻿// WorkWithStrings();
+Generate20Fibonacci();
+
+void Generate20Fibonacci()
+{
+    List<int> fibonacciNumbers = [1, 1];
+    Console.Write("The Fibonacci sequence is (first 20): 1, 1");
+    int count;
+    while (fibonacciNumbers.Count < 20){
+        count = fibonacciNumbers.Count;
+        fibonacciNumbers.Add(
+            fibonacciNumbers[count - 1] + fibonacciNumbers[count - 2]
+        );
+        Console.Write($", {fibonacciNumbers[fibonacciNumbers.Count - 1]}");
+    }
+    Console.WriteLine();
+    string e = Console.ReadLine("How are you? ");
+    Console.WriteLine(e);
+}
+
+void WorkWithStrings()
+{
+    List<string> names = ["Daniel", "Ana", "Felipe"];
+    foreach (var name in names)
+    {
+        Console.WriteLine($"Hello {name.ToUpper()}!");
+    }
+    Console.WriteLine();
+    names.Add("Maria");
+    names.Add("Bill");
+    names.Remove("Ana");
+    foreach (var name in names)
+    {
+        Console.WriteLine($"Hello {name.ToUpper()}!");
+    }
+
+    Console.WriteLine($"\nMy name is {names[0]}");
+    Console.WriteLine($"I've added {names[2]} and {names[3]} to the list");
+
+    Console.WriteLine($"\nThe list has {names.Count} people in it");
+
+    var index = names.IndexOf("Felipe");
+    if (index != -1)
+    {
+        Console.WriteLine($"\nThe name {names[index]} is at index {index}");
+    }
+    else
+    {
+        Console.WriteLine("The name Felipe is not in the list");
+    }
+
+    index = names.IndexOf("Not Found");
+    if (index == -1)
+    {
+        Console.WriteLine($"\nWhen an item is not found, IndexOf returns {index}");
+    }
+    else
+    {
+        Console.WriteLine($"The name {names[index]} is at index {index}");
+    }
+
+    names.Sort();
+    foreach (var name in names)
+    {
+        Console.WriteLine($"Hello {name.ToUpper()}!");
+    }
+}
---
+++ learning/numbers-quickstart/Program.cs
@@ -0,0 +1,124 @@
+﻿#pragma warning disable CS8321
+// WorkWithIntegers();
+// OrderPrecedence();
+// TestLimits();
+// WorkWithDoubles();
+// WorkWithDecimals();
+PiFinder();
+
+
+void PiFinder()
+{
+    double pi = Math.PI;
+    double radius = 2.5;
+    double area = pi * Math.Pow(radius, 2);
+    Console.WriteLine(area);
+}
+
+void WorkWithDecimals()
+{
+    decimal min = decimal.MinValue;
+    decimal max = decimal.MaxValue;
+    Console.WriteLine($"The range of decimal is {min} to {max}");
+
+    double a = 1.0;
+    double b = 3.0;
+    Console.WriteLine(a / b);
+
+    decimal c = 1.0M;
+    decimal d = 3.0M;
+    Console.WriteLine(c / d);
+
+    decimal e = (decimal)(a / b);
+    Console.WriteLine(e);
+}
+
+void WorkWithDoubles()
+{
+    double a = 5;
+    double b = 4;
+    double c = 2;
+    double d = (a + b) / c;
+    Console.WriteLine(d);
+
+    double e = 19;
+    double f = 23;
+    double g = 8;
+    double h = (e + f) / g;
+    Console.WriteLine(h);
+
+    // get min and max of double
+    double max = double.MaxValue;
+    double min = double.MinValue;
+    Console.WriteLine($"The range of double is {min} to {max}");
+
+    // rounding errors?
+    double third = 1.0 / 3.0;
+    Console.WriteLine(third);
+}
+
+void TestLimits()
+{
+    int a = 7;
+    int b = 4;
+    int c = 3;
+    int d = (a + b) / c;
+    int e = (a + b) % c;
+    Console.WriteLine($"quotient: {d}");
+    Console.WriteLine($"remainder: {e}");
+
+    // printing out the maximum and minimum values for integers
+    int max = int.MaxValue;
+    int min = int.MinValue;
+    Console.WriteLine($"The range of integers is {min} to {max}");
+
+    // checking max value wrapping
+    int what = max + 3;
+    Console.WriteLine($"An example of overflow: {what}");
+    // testing with multiplication by 2
+    what = max * 2;
+    Console.WriteLine($"Another example of overflow: {what}");
+}
+
+void WorkWithIntegers()  // 
+{
+    // addition
+    int a = 18;
+    int b = 6;
+    int c = a + b;
+    Console.WriteLine(c);
+
+    // subtraction
+    c = a - b;
+    Console.WriteLine(c);
+
+    // multiplication
+    c = a * b;
+    Console.WriteLine(c);
+
+    // division
+    c = a / b;
+    Console.WriteLine(c);
+}
+
+void OrderPrecedence()
+{
+    int a = 5;
+    int b = 4;
+    int c = 2;
+    int d = a + b * c;
+    Console.WriteLine(d);
+
+    d = (a + b) * c;
+    Console.WriteLine(d);
+
+    d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+    Console.WriteLine(d);
+
+    int e = 7;
+    int f = 4;
+    int g = 3;
+    int h = (e + f) / g;
+    Console.WriteLine(h);
+    Console.WriteLine(8/3);
+}
