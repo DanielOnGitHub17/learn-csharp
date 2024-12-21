f6810a3a3f203a76239c9e8f9d731291709185a9
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2024-12-20 22:27:56 -0600

    Add initial project files and main structure for play application

---
+++ play/Program.cs
@@ -0,0 +1,21 @@
+﻿// generate initial main structure
+using System;
+
+namespace play
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // foreach (var arg in args){
+            //     Console.Write($"{arg} ");
+            // }
+            // Console.WriteLine("\nHello World!");
+            long i = 0;
+            while (i < Math.Pow(10, 9)){
+                i++;
+            }
+            Console.WriteLine("Done!");
+        }
+    }
+}
